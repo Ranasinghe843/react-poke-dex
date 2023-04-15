adbf1e8a3cfc35a619e7d310c79fcf1596f27ac5
Ranasinghe843 <ranasinghe843@gmail.com>
2023-04-14 21:34:14 -0400

    Updates

---
+++ precache-manifest.34a2c49770beddd7cfd0048acb118234.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f01ecbc0fedae6d1409",
+    "url": "/react-poke-dex/static/css/main.5e63a888.chunk.css"
+  },
+  {
+    "revision": "5f01ecbc0fedae6d1409",
+    "url": "/react-poke-dex/static/js/main.5f01ecbc.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/react-poke-dex/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/react-poke-dex/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "e9d35e8a38bf42ad925d",
+    "url": "/react-poke-dex/static/js/runtime~main.e9d35e8a.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-poke-dex/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ff6e521d0ea2966bcf4fed04393ace86",
+    "url": "/react-poke-dex/index.html"
+  }
+];
